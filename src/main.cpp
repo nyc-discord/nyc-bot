cc7b3585fac94304630d8b476d36c3d4504dd7f0
Ariel Saldana <ariel@unreal.io>
2023-05-27 11:08:57 -0400

    Dropdown POC for selecting Boroughs

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "bot-instance.h"
 #include "commands.h"
+#include <dpp/dpp.h>
 
 int
 main()
@@ -17,6 +18,37 @@ main()
             }
         }
     });
+
+    /// custom code put this in a file
+    bot->on_message_create([&bot](const dpp::message_create_t& event) {
+        if (event.msg.content == "!select")
+        {
+            /* Create a message containing an action row, and a select menu within the action row. */
+            dpp::message m(event.msg.channel_id, "Which borough are you from?");
+            m.add_component(dpp::component().add_component(
+                dpp::component()
+                    .set_type(dpp::cot_selectmenu)
+                    .set_placeholder("Pick a borough")
+                    .add_select_option(dpp::select_option("Bronx", "bronx", "Land of rats").set_emoji("😄"))
+                    .add_select_option(dpp::select_option("Manhattan", "manhattan", "Best Borough").set_emoji("🙂"))
+                    .add_select_option(dpp::select_option("Queens", "queens", "Who even lives here").set_emoji("🙂"))
+                    .add_select_option(dpp::select_option("Brooklyn", "brooklyn", "Better be Williamsburg").set_emoji("🙂"))
+                    .add_select_option(dpp::select_option("Staten Island", "statenisland", "Ok fed").set_emoji("🙂"))
+                    .set_id("nyc_borough")));
+            bot->message_create(m);
+        }
+    });
+    /* When a user clicks your select menu , the on_select_click event will fire,
+     * containing the custom_id you defined in your select menu.
+     */
+    bot->on_select_click([&bot](const dpp::select_click_t& event) {
+        /* Select clicks are still interactions, and must be replied to in some form to
+         * prevent the "this interaction has failed" message from Discord to the user.
+         */
+        event.reply("You clicked " + event.custom_id + " and chose: " + event.values[0]);
+    });
+
+    /// end custom code
 
     bot->on_log(dpp::utility::cout_logger());
     bot_instance.start_bot();
