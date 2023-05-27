8d15a1d95045d25f490015bf50e2145ed6e2c210
Ariel Saldana <ariel@unreal.io>
2023-05-27 17:00:37 -0400

    Refactor files

--- include/commands.h
+++ include/commands.h
@@ -5,8 +5,8 @@
 #ifndef NYC_DISCORD_BOT_COMMANDS_H
 #define NYC_DISCORD_BOT_COMMANDS_H
 
+#include "../src/command-announcement.h"
 #include "command-about.h"
-#include "command-announcement.h"
 #include "command-ping.h"
 
 #endif // NYC_DISCORD_BOT_COMMANDS_H
